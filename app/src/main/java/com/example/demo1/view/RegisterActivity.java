394a18cc52905029a5ae995c69c42c3e05f7474e
Quốc Nguyễn Minh <102200149@sv1.dut.udn.vn>
2023-04-06 13:50:51 +0700

    mquoc ud

--- app/src/main/java/com/example/demo1/view/RegisterActivity.java
+++ app/src/main/java/com/example/demo1/view/RegisterActivity.java
@@ -1,16 +1,67 @@
 package com.example.demo1.view;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 
-import com.example.demo1.R;
+import com.example.demo1.base.BaseActivity;
+import com.example.demo1.core.register.RegisterContract;
+import com.example.demo1.core.register.RegisterPresenter;
+import com.example.demo1.databinding.ActivityRegisterBinding;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends BaseActivity implements RegisterContract.View {
+
+    ActivityRegisterBinding binding;
+    RegisterContract.Presenter registerPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        binding = ActivityRegisterBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        initViews();
+    }
+
+    private void initViews() {
+        registerPresenter = new RegisterPresenter(this);
+
+        binding.btRegister.setOnClickListener(v -> registerPresenter.register(
+                binding.etUsername.getText().toString(),
+                binding.etEmail.getText().toString(),
+                binding.etPassword.getText().toString(),
+                binding.etRepeatPassword.getText().toString()
+        ));
+    }
+
+    @Override
+    public void setUsernameError(int error) {
+        binding.etUsername.setError(getString(error));
+    }
+
+    @Override
+    public void setEmailError(int error) {
+        binding.etEmail.setError(getString(error));
+    }
+
+    @Override
+    public void setPasswordError(int error) {
+        binding.etPassword.setError(getString(error));
+    }
+
+    @Override
+    public void setRepeatedPasswordError(int error) {
+        binding.etRepeatPassword.setError(getString(error));
+    }
+
+    @Override
+    public void onRegisterSuccess(int message) {
+        showMessage(message);
+        Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onRegisterFailure(String message) {
+        showMessage(message);
     }
 }
